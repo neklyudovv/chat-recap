056803960cdd7792f21d3805205fc7206d815ad5
Igor Nekludov <igor.nekludovv@gmail.com>
2025-01-16 21:22:33 +0300

    small details

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def pr_word_stats(word_stats):
     s = '\n'
     for i, word in enumerate(word_stats):
-        if i<10:
+        if i < 10:
             s += f'{word} {word_stats[word]}\n'
     return s
