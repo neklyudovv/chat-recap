97c1396ac96b1f1a27b1a67e3c86240e14479911
Igor Nekludov <igor.nekludovv@gmail.com>
2025-01-27 12:30:36 +0300

    moved data loading and validation from main

---
+++ data_loader.py
@@ -0,0 +1,15 @@
+import json
+
+
+def load_chat_data(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        if "messages" not in data or "name" not in data:
+            raise ValueError("wrong data format")
+        return data
+
+    except FileNotFoundError:
+        return ValueError("no such file")
+    except json.JSONDecodeError:
+        raise ValueError("incorrect json")
