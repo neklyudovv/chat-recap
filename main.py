28322db8fb137871d568b4425f09e2607f4f584c
Igor Nekludov <igor.nekludovv@gmail.com>
2025-03-24 23:28:55 +0300

    removed words and emojis cutting (it still does but in analyzer now)

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ from visual import render_recap
 
 
 def main():
-    data = load_chat_data("result.json")
+    data, chatter_name = load_chat_data("result.json")
 
-    analyzer = ChatAnalyzer(data)
+    analyzer = ChatAnalyzer(data, chatter_name)
     results = analyzer.get_stats()
     # print(results)  # по сути results это уже готовый ответ, по которому можно рисовать картинку
     # / отдавать на сайт в качестве ответа
 
     print('chatter:', analyzer.chatter_name)
-    print('total messages:', len(analyzer.messages))
-    print(f'most used words:\n{analyzer.your_name}: {results["your_word_stats"][:10]}\n'
-          f'{analyzer.chatter_name}: {results["chatter_word_stats"][:10]}')
+    print('total messages:', len(analyzer.df))
+    print(f'most used words:\n{analyzer.your_name}: {results["your_word_stats"]}\n'
+          f'{analyzer.chatter_name}: {results["chatter_word_stats"]}')
     print(f'messages sent:\n{analyzer.your_name}: {results["your_messages_count"]}\n'
           f'{analyzer.chatter_name}: {results["chatter_messages_count"]}')
     print(f'avg response times (s):\n{analyzer.your_name}: {results["your_average_time"]}\n'
           f'{analyzer.chatter_name}: {results["chatter_average_time"]}')
-    print(f'most used emojis:\n{analyzer.your_name}: {results["your_emojis"][:10]}\n'
-          f'{analyzer.chatter_name}: {results["chatter_emojis"][:10]}')
+    print(f'most used emojis:\n{analyzer.your_name}: {results["your_emojis"]}\n'
+          f'{analyzer.chatter_name}: {results["chatter_emojis"]}')
     print(f'most active month: from {results["most_active_month"]["start"]} to '
           f'{results["most_active_month"]["end"]} with msg count of {results["most_active_month"]["messages_count"]}')
 
     # с генерацией картинки надо придумать что-то явно получше
     # а пока просто повисит закомменченная, я тут архитектуру перестраиваю
-    render_recap(analyzer.your_name, analyzer.chatter_name, results['your_messages_count'],
-                 results['chatter_messages_count'], results['your_average_time'],
-                 results['chatter_average_time'], results['your_word_stats'],
-                 results['chatter_word_stats'], results['your_emojis'][:10], results['chatter_emojis'][:10])
+    #render_recap(analyzer.your_name, analyzer.chatter_name, results['your_messages_count'],
+    #             results['chatter_messages_count'], results['your_average_time'],
+    #             results['chatter_average_time'], results['your_word_stats'],
+    #             results['chatter_word_stats'], results['your_emojis'], results['chatter_emojis'])
 
 
 if __name__ == "__main__":
